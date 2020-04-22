0a4daee1f687399d00e4da4280e0c315ee3326d9
Agustin Gonzalez <37369252+Aguspeedy@users.noreply.github.com>
2020-04-22 10:34:23 -0300

    Login form implemented, styling missing (#2)
    * Login form implemented, styling missing
    
    * Fixed validations, styles and comments

--- uid-client/src/App.js
+++ uid-client/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  return <div>UID</div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 }
 
 export default App;
