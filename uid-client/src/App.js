b7dcbe7c0cce37f9d9290171919fb8394e0e3473
Diego Lauz <diego.lauz@hotmail.com>
2020-04-23 19:29:29 -0300

    Register Page (#3)

--- uid-client/src/App.js
+++ uid-client/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import LoginForm from "./components/LoginForm";
+import React from 'react';
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <div>
-      <LoginForm />
+      <RegisterForm />
     </div>
   );
 }
