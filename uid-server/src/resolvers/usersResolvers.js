1619c385e118bbdde494ae2d008b83bb58c0c1d3
Diego Lauz <diego.lauz@hotmail.com>
2020-05-06 13:13:14 -0300

    Signup functionality completed using hashing and jwt (#10)

--- uid-server/src/resolvers/usersResolvers.js
+++ uid-server/src/resolvers/usersResolvers.js
@@ -1,14 +1,21 @@
 import { User } from '../models';
 import { UserModel } from '../data';
+import { hash } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { pick } from 'lodash';
 
 export const usersResolvers = {
   Query: {
     users: () => User.all(),
   },
   Mutation: {
-    signUp: (_, { user }) => {
-      UserModel.create([user]);
-      return `${user.firstName} ${user.lastName} (${user.email}) <${user.password}>`;
+    signUp: async (_, { user }) => {
+      const passwordHash = await hash(user.password, 10);
+      const userToSave = { ...pick(user, ['email', 'firstName', 'lastName']), passwordHash };
+      const [createdUser] = await UserModel.create([userToSave]);
+      const jwtPayload = { user: pick(createdUser, ['email', 'firstName', 'lastName']) };
+      const jwt = sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '1d' });
+      return jwt;
     },
   },
 };
