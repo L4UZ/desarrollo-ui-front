1d8337ca66446439f1b0d08925677a1e2f803bf7
Diego Lauz <diego.lauz@hotmail.com>
2020-05-07 17:28:17 -0300

    Configuration files (#12)

--- uid-server/webpack.config.js
+++ uid-server/webpack.config.js
@@ -1,15 +1,27 @@
+const path = require('path');
+
 module.exports = {
-  rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-proposal-object-rest-spread'],
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
         },
       },
-    },
-  ],
+    ],
+  },
 };
