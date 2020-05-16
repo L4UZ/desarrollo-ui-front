cd39e497b432424f33004ccefaa3e6bfa8a613ad
Agustin Gonzalez <agonzale_95@hotmail.com>
2020-05-16 18:23:37 -0300

    Minior fixes

--- src/pages/RegionDetail/index.js
+++ src/pages/RegionDetail/index.js
@@ -17,7 +17,7 @@ const RegionDetail = () => {
 
   return (
     <Container>
-      <Typography variant="h2">{regionName}</Typography>
+      <Typography variant="h2">{region.name}</Typography>
       <Grid container spacing={3}>
         {region.places.map(place => (
           <PlaceItem key={place.id} place={place} onClick={() => setSelectedPlace(place)} />
