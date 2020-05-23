cd047f051dabb4df18b2ecbd1809695951d2bbd0
Diego Lauz <diego.lauz@hotmail.com>
2020-05-23 19:31:38 -0300

    Add review (#23)

--- src/components/PlaceDetail/AddReview/index.js
+++ src/components/PlaceDetail/AddReview/index.js
@@ -1,96 +1,131 @@
 import React from 'react';
-import { Grid, Typography, Button, TextField } from '@material-ui/core';
+import { Grid, Typography, Button, TextField, Paper } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 import { Formik } from 'formik';
 import { useMutation } from '@apollo/react-hooks';
 import { string } from 'prop-types';
+import { useHistory } from 'react-router-dom';
 
+import { ADD_REVIEW_LOCATION } from '../../../constants/localStorageKeys';
+import { useAuth } from '../../../AuthProvider/index';
 import useStyles from './styles';
 import { ADD_REVIEW_MUTATION } from '../../../api/mutations';
+import { PLACE_DETAIL } from '../../../api/queries';
 
-const AddReview = ({ placeId }) => {
+const AddReview = ({ regionId, placeId }) => {
   const classes = useStyles();
-  const [addReview, { data, loading, error }] = useMutation(ADD_REVIEW_MUTATION);
+  const { push: pushToHistory } = useHistory();
+  const { token } = useAuth();
 
-  // TODO: Require logged in user to add review
-  // TODO: Add data to local state
-  // TODO: Handle error
+  const [addReview, { loading }] = useMutation(ADD_REVIEW_MUTATION, {
+    update(cache, { data: { addReview: addedReview } }) {
+      const { place } = cache.readQuery({ query: PLACE_DETAIL, variables: { placeId } });
+      cache.writeQuery({
+        query: PLACE_DETAIL,
+        variables: { placeId },
+        data: { place: { ...place, reviews: [...place.reviews, addedReview] } },
+      });
+    },
+  });
+
+  const redirectToLogin = () => {
+    localStorage.setItem(ADD_REVIEW_LOCATION, `region-detail/${regionId}/${placeId}`);
+    pushToHistory('/signin');
+  };
 
   return (
     <Grid item xs={12} className={classes.gridItem}>
-      <Typography variant="h6" gutterBottom>
-        Add review
-      </Typography>
-      <Formik
-        initialValues={{ score: 0, comment: '' }}
-        onSubmit={({ comment, score }, { resetForm }) => {
-          addReview({
-            variables: {
-              review: {
-                comment,
-                score: +score,
-                placeId,
-                // token,
+      <Paper elevation={2} className={classes.container}>
+        <Typography variant="h6" gutterBottom>
+          Add review
+        </Typography>
+        <Formik
+          initialValues={{ score: 0, comment: '' }}
+          onSubmit={({ comment, score }, { resetForm }) => {
+            addReview({
+              variables: {
+                review: {
+                  comment,
+                  score: +score,
+                  placeId,
+                  token,
+                },
               },
-            },
-          });
-          resetForm();
-        }}
-      >
-        {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit} className={classes.form}>
-            <Grid container>
-              <Grid item xs={12}>
-                <Rating
-                  defaultValue={0}
-                  id="score"
-                  name="score"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  size="large"
-                />
+            });
+            resetForm({ values: { score: 0 } });
+          }}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+            <form onSubmit={handleSubmit} className={classes.form}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Rating
+                    defaultValue={0}
+                    id="score"
+                    name="score"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    size="large"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    error={errors.comment && touched.comment}
+                    fullWidth
+                    helperText={touched.comment && errors.comment}
+                    id="comment"
+                    label="Comment"
+                    margin="normal"
+                    multiline
+                    name="comment"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    placeholder="Add a review"
+                    required
+                    type="text"
+                    value={values.comment}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} className={classes.buttonWrapper}>
+                  <Button
+                    className={classes.submit}
+                    color="primary"
+                    disabled={loading}
+                    fullWidth
+                    type="submit"
+                    variant="contained"
+                  >
+                    Submit
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  error={errors.comment && touched.comment}
-                  fullWidth
-                  helperText={touched.comment && errors.comment}
-                  id="comment"
-                  label="comment"
-                  margin="normal"
-                  multiline
-                  name="comment"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  placeholder="Add a review"
-                  required
-                  type="text"
-                  value={values.comment}
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={12} className={classes.buttonWrapper}>
-                <Button
-                  className={classes.submit}
-                  color="primary"
-                  disabled={loading}
-                  fullWidth
-                  type="submit"
-                  variant="contained"
-                >
-                  Submit
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
+            </form>
+          )}
+        </Formik>
+        {!token && (
+          <div className={classes.overlay}>
+            <Button
+              className={classes.overlayButton}
+              color="secondary"
+              disabled={loading}
+              fullWidth
+              type="submit"
+              variant="contained"
+              onClick={redirectToLogin}
+            >
+              Sign in
+            </Button>
+          </div>
         )}
-      </Formik>
+      </Paper>
     </Grid>
   );
 };
 
 AddReview.propTypes = {
   placeId: string.isRequired,
+  regionId: string.isRequired,
 };
 
 export default AddReview;
